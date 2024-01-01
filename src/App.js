20aeeb61d4262d11ab8065ce637f66baa9704a1e
akshaysable02 <akshaysable097@gmail.com>
2024-01-01 10:14:20 +0530

    Compose and send Mail

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginForm from "./Components/Auth/LoginForm";
-import Welcome from "./Components/Mail/Welcome";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Mailbox from "./Components/Mail/Mailbox";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useSelector } from "react-redux";
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  console.log("isLoggedIn", isLoggedIn);
 
   return (
     <BrowserRouter>
       <Routes>
-        {!isLoggedIn && <Route path="/" element={<LoginForm />} />}
-        {isLoggedIn ? (
-          <Route path="/welcome" element={<Welcome />} />
-        ) : (
-          <Route path="*" element={<Navigate to="/" />} />
-        )}
+        {!isLoggedIn && <Route path="/" element={<LoginForm />} exact />}
+        {isLoggedIn && <Route path="/" element={<Mailbox/>} />}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Components/Mail/Welcome.js
+++
@@ -1,7 +0,0 @@
-const Welcome = () => {
-      return (
-            <h1 className="text-centre">Welcome to your mail box</h1>
-      );
-};
-
-export default Welcome;
